6b3f96544cb6837b4cd4410ee3f4818e644fced0
Konynico <thenicodu74@gmail.com>
2023-06-18 11:10:19 +0200

    Save du graph sans import

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,37 @@ import Controller.GraphLoader;
 import Model.Graph;
 import View.GraphVisualizer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 
 public class Main {
     public static void main(String[] args) {
-        InputStream resourceStream = Main.class.getResourceAsStream("/Resources/graph.csv");
+
+        InputStream resourceStream = null;
+
+
+        // Si un fichier graph.csv existe dans le dossier Documents de l'utilisateur, on le charge
+
+        String filePath = System.getProperty("user.home") + "/Documents/graph.csv";
+        File file = new File(filePath);
+
+        if (file.exists()) {
+            try {
+                resourceStream = new FileInputStream(file);
+            } catch (FileNotFoundException e) {
+                System.out.println("Impossible de charger le fichier graph.csv");
+            }
+        } else {
+            resourceStream = Main.class.getResourceAsStream("/Resources/graph.csv");
+        }
+
+
+
+
+
         Graph graph;
 
         if (resourceStream != null) {
